933958f5b164b4482c26e512c2f9eeb23b0d17b7
Rifky Albuchori <rifkyalbuchorilearning@gmail.com>
2026-04-15 08:01:47 +0700

    Menambahkan class ListFavorit_103022400126

--- mod7_103022400058/Program.cs
+++ mod7_103022400058/Program.cs
@@ -43,13 +43,62 @@ namespace mod7_103022400058
 
             }
         }
-    }
-    public class program
-    {
-        public static void Main(String[] args)
+
+        public class ListFavorit_103022400126
         {
-            FilmFavorit_103022400126 film = new FilmFavorit_103022400126();
-            film.readJSON();
+            public class watchList
+            {
+                public string watchListName { get; set; }
+                public string createdBy { get; set; }
+                public List<movie> movies { get; set; }
+            }
+            public class movie
+            {
+
+                public string id { get; set; }
+                public string title { get; set; }
+                public int year { get; set; }
+                public string genre { get; set; }
+                public double rating { get; set; }
+            }
+
+            public void readJSON()
+            {
+                string filepath = "D:\\CODE\\Modul7_Kelompok5\\mod7_103022400058\\jurnal7_2_103022400126.json";
+
+                try
+                {
+                    if (!File.Exists(filepath))
+                    {
+                        Console.WriteLine($"File {filepath} tidak ditemukan!");
+                        return;
+                    }
+
+                    string jsonString = File.ReadAllText(filepath);
+                    watchList film = JsonSerializer.Deserialize<watchList>(jsonString);
+                    Console.WriteLine($"WatchList name : {film.watchListName} \nCreated BY : {film.createdBy} \nMovies : ");
+                    for (int i = 0; i < film.movies.Count; i++)
+                    {
+                        Console.WriteLine(film.movies[i].id + " " + film.movies[i].title + " " + film.movies[i].genre + " (" + film.movies[i].year + " - " + film.movies[i].rating + ")");
+
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+
+                }
+            }
+        }
+        public class program
+        {
+            public static void Main(String[] args)
+            {
+                FilmFavorit_103022400126 film = new FilmFavorit_103022400126();
+                film.readJSON();
+                ListFavorit_103022400126 favorit_103022400126 = new ListFavorit_103022400126();
+                favorit_103022400126.readJSON();
+            }
         }
     }
 }
