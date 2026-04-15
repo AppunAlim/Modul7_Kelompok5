83ab828077375e8db8b4d1e1db6e019df1dd7ab0
AppunAlim <affansalim.xpro@gmail.com>
2026-04-15 08:10:33 +0700

    menambahkan class WatchList

--- mod7_103022400058/Program.cs
+++ mod7_103022400058/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System.Text.Json;
+using static mod7.FilmFavorit_103022400058;
+using static mod7.WatchList_103022400058;
 
 namespace mod7
 {
@@ -15,6 +17,7 @@ namespace mod7
             public bool isWatched { get; set; }
         }
 
+
         public void readJson()
         {
             string f = "D:\\mod7_103022400058\\mod7_103022400058\\jurnal7_1_103022400058.json";
@@ -30,12 +33,61 @@ namespace mod7
         }
     }
 
+    public class WatchList_103022400058
+    {
+        public class WatchList
+        {
+            public string watchListName { get; set; }
+            public string createdBy { get; set; }
+            public List<Movie> movies { get; set; }
+        }
+        public class Movie
+        {
+
+            public string id { get; set; }
+            public string title { get; set; }
+            public int year { get; set; }
+            public string genre { get; set; }
+            public double rating { get; set; }
+        }
+
+        public void ReadJson()
+        {
+            string filepath = "D:\\mod7_103022400058\\mod7_103022400058\\jurnal7_2_103022400058.json";
+            try
+            {
+                if (!File.Exists(filepath))
+                {
+                    Console.WriteLine($"File {filepath} tidak ditemukan!");
+                    return;
+                }
+
+                string jsonString = File.ReadAllText(filepath);
+                WatchList film = JsonSerializer.Deserialize<WatchList>(jsonString);
+                Console.WriteLine($"WatchList name : {film.watchListName} \nCreated BY : {film.createdBy} \nMovies : ");
+                for (int i = 0; i < film.movies.Count; i++)
+                {
+                    Console.WriteLine(film.movies[i].id + " " + film.movies[i].title + " " + film.movies[i].genre + " (" + film.movies[i].year + " - " + film.movies[i].rating + ")");
+
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+
+            }
+        }
+    }
+
     public class Program
     {
         public static void Main(string[] args)
         {
             FilmFavorit_103022400058 film = new FilmFavorit_103022400058();
             film.readJson();
+            Console.WriteLine("\nD");
+            WatchList_103022400058 wl = new WatchList_103022400058();
+            wl.ReadJson();
         }
     }
 }
